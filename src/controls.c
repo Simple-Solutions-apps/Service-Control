e4b3eb6c3517a4273b854a4ad3efed1ea64ebab1
Simple-Solutions-apps <simple.solutions.apps@gmail.com>
2023-01-25 17:09:09 -0600

    Automated commit(s)

--- src/controls.c
+++ src/controls.c
@@ -397,8 +397,8 @@ int CreateControls(HWND sHwndMain)
 	wcscpy_s(cComboOptions[4], 15, TEXT("filesys"));
 	wcscpy_s(cComboOptions[5], 15, TEXT("rec"));
 	wcscpy_s(cComboOptions[6], 15, TEXT("interact"));
-	wcscpy_s(cComboOptions[7], 15, TEXT("adapt"));
-	for(iLoopIndex = 0; iLoopIndex <= 7; iLoopIndex++)
+	wcscpy_s(cComboOptions[8], 15, TEXT("adapt"));
+	for(iLoopIndex = 0; iLoopIndex <= 8; iLoopIndex++)
 	{
 		SendMessage(sHwndCtlCmbSvcType,(UINT) CB_ADDSTRING,(WPARAM) 0,(LPARAM) cComboOptions[iLoopIndex]);
 	}
